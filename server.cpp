4299595b91b8d48eb76d70413b3f8009916e318a
George <gnasour@gmail.com>
2022-01-04 13:23:51 -0800

    Finishing up server implementation

--- server.cpp
+++ server.cpp
@@ -24,31 +24,18 @@ int main(int argc, char* argv[]){
   while(1){
     newfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
     if(!fork()){
-      fprintf(stderr, "Its open\n");
-      exit(1);
+      
     }else{
       printf("Parent process, closing socket\n");
-      close(newfd);
+      close(sockfd);
       exit(1);
     }
   }
-  /*  for(p = res; p != NULL; p= p -> ai_next){
-    void* addr;
-    char* ipver;
-
-    if(p->ai_family == AF_INET){
-      struct sockaddr_in *ipv4 = (struct sockaddr_in *) p->ai_addr;
-      addr = &(ipv4->sin_addr);
-      ipver = "IPv4";
-    }else{
-      struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *) p->ai_addr;
-      addr = &(ipv6->sin6_addr);
-      ipver = "IPv6";
-    }
-    inet_ntop(p->ai_family, addr, buff, sizeof buff);
-    printf("%s: %s\n", ipver, buff);
-
-    }*/
-  printf("%d\n", sockfd);
-
 }
+
+
+int init(int sock){
+  int oxy_val = 0;
+  while(1){
+    read(sock, &oxy_val, sizeof int);
+  }
