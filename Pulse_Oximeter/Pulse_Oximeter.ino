f17674dd85067acf2951f4e8142995cd904efd6c
gnasour <gnasour@gmail.com>
2022-04-01 14:50:03 -0700

    ESP8266 synced with server to transmit the medical data

--- Pulse_Oximeter/Pulse_Oximeter.ino
+++ Pulse_Oximeter/Pulse_Oximeter.ino
@@ -1,38 +1,41 @@
-#include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-const uint16_t port = 1025;
-const char *host = "192.168.1.199";
-WiFiClient client;
-void setup()
-{
-    Serial.begin(115200);
-    WiFi.mode(WIFI_STA);
-    WiFi.begin("Nasour", "Hn4281992@#"); // change it to your ussid and password
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(500);
-    }
-    Serial.println(WiFi.localIP());
-}
-
-void loop()
-{
-    while (!client.connect(host, port))
-    {
-       Serial.println("Could not connect to the client");
-       delay(1000);
-    }
-    
-     while (client.available() > 0)
-    {
-         client.println("Hello From ESP8266");
-         delay(5000);
- 
-//        char c = client.read();
-//        client.println(c);  
-      }
-    
-    client.stop();
-    delay(5000);
-}
+//#include <Arduino.h>
+//#include <ESP8266WiFi.h>
+//#include <WiFiClient.h>
+//const uint16_t port = 1025;
+//const char *host = "192.168.1.199";
+//WiFiClient client;
+//void setup()
+//{
+//    Serial.begin(115200);
+//    WiFi.mode(WIFI_STA);
+//    WiFi.begin("Nasour", "Hn4281992@#"); // change it to your ussid and password
+//    Serial.println("He");
+//    while (WiFi.status() != WL_CONNECTED)
+//    {
+//      Serial.println("Cant connect");
+//      delay(500);
+//    }
+//    Serial.println(WiFi.localIP());
+//}
+//
+//void loop()
+//{
+//    while (!client.connect(host, port))
+//    {
+//       Serial.println("Could not connect to the client");
+//       delay(1000);
+//    }
+//
+// 
+//     while (client.connected())
+//    {
+//          if(Serial.available()){
+//            client.write(Serial.read()); 
+//         }
+////        char c = client.read();
+////        client.println(c);  
+//      }
+//    
+//    client.stop();
+//    delay(5000);
+//}
