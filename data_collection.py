28fabcc98efaae5221d4950f337ebb2363c9ca1c
George <gnasour@gmail.com>
2022-05-17 23:17:02 -0700

    Synced data_collection.py and processing data correctly

--- data_collection.py
+++ data_collection.py
@@ -1,11 +1,16 @@
+#!/usr/bin/python3
+
 import socket
-import sys
 import os
 
+COLLECT_AMT = 250
+FIFO = "myfifo"
 server_address = './Sp_data'
 
+#Write all incoming data
 fw = open("data.txt", "w")
 
+#Create the Unix Domain Socket
 try:
     os.unlink(server_address)
 except OSError:
@@ -13,10 +18,27 @@ except OSError:
         print("No file")
         raise
 
+#Connect to the socket
 sock = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
-
 sock.bind(server_address)
 sock.listen(1)
-
 print('waiting for a connection')
 connection, client_address = sock.accept()
+
+#Amount of recorded data received from UDS
+rcrd_count = 0
+print('Connection accepted. Begin data collection.')
+while True:
+    if rcrd_count < COLLECT_AMT:
+        data = connection.recv(128)
+        if data:
+            fw.write(data.decode("utf-8"))
+            rcrd_count+=1
+    else:
+        fw.flush()
+        data_flag = open(FIFO, "w")
+        data_flag.write("1")
+        data_flag.flush()
+        rcrd_count = 0
+            
+    
--- data_process.py
+++ data_process.py
@@ -1,21 +1,25 @@
 import sys
 import os
+import signal
 import time
+import re
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
 FIFO = "myfifo"
 
-try:
+#Make the named pipe
+if not os.path.exists(FIFO):
     os.mkfifo(FIFO)
-except:
-    pass
 
-data_signal = open(FIFO)
-
-if os.fork() == 0:
+#Start the data collection process
+child_pid = os.fork()
+if child_pid == 0:
     os.execvp("./data_collection.py", sys.argv)
+
+#IPC to signal data collection is done
+data_flag = open(FIFO)
 
 #Plot package
 plt.style.use('ggplot')
@@ -25,9 +29,8 @@ fr = open("data.txt", "r")
 
 #Gradient Approx. Constants
 heart_rate_span = [10,250]
-pts = 2000
+pts = 250
 smoothing_size = 20
-
 
 
 fig = plt.figure(figsize=(14,8))
@@ -43,86 +46,76 @@ ax1.tick_params(axis='both',which='major',labelsize=16)
 while True:
     t_vec, ir_vec, red_vec, y_vals=[],[],[],[]
     try:
-        try: 
-            while len(y_vals)<pts:
-                data = connection.recv(128)
-                if data:
-                    fw.write(data.decode("utf-8"))
-                    fw.flush()
-                    #curr_line = data.decode("utf-8")
-                    #curr_data = re.split(":|,", curr_line[0:-1])
-                    # if len(curr_data)==3:
-                    #     try:
-                    #         t_vec.append(float(curr_data[0])/1000000.0)
-                    #         y_vals.append(float(curr_data[2]))
-                            
-                    #     except:
-                    #         continue
-            t1 = time.time()
-            samp_rate = 1/np.mean(np.diff(t_vec))
-            min_time_bw_samps = (60.0/heart_rate_span[1])
-            y_vals = np.convolve(y_vals, np.ones((smoothing_size,)),'same')/smoothing_size
-            red_grad = np.gradient(y_vals,t_vec)
-            red_grad[0:int(smoothing_size/2)+1] = np.zeros((int(smoothing_size/2)+1,))
-            red_grad[-int(smoothing_size/2)-1:] = np.zeros((int(smoothing_size/2)+1,))
+        if data_flag.read(1):
+            for i in range(pts):
+                curr_line = fr.readline()
+                curr_data = re.split(":|,", curr_line[0:-1])
+                if len(curr_data)==3:
+                    try:
+                        t_vec.append(float(curr_data[0])/1000000.0)
+                        y_vals.append(float(curr_data[2]))
+                    except:
+                        continue
+        t1 = time.time()
+        samp_rate = 1/np.mean(np.diff(t_vec))
+        min_time_bw_samps = (60.0/heart_rate_span[1])
+        y_vals = np.convolve(y_vals, np.ones((smoothing_size,)),'same')/smoothing_size
+        red_grad = np.gradient(y_vals,t_vec)
+        red_grad[0:int(smoothing_size/2)+1] = np.zeros((int(smoothing_size/2)+1,))
+        red_grad[-int(smoothing_size/2)-1:] = np.zeros((int(smoothing_size/2)+1,))
 
-            y_vals = np.append(np.repeat(y_vals[int(smoothing_size/2)],int(smoothing_size/2)),y_vals[int(smoothing_size/2):-int(smoothing_size/2)])
-            y_vals = np.append(y_vals,np.repeat(y_vals[-int(smoothing_size/2)],int(smoothing_size/2)))
+        y_vals = np.append(np.repeat(y_vals[int(smoothing_size/2)],int(smoothing_size/2)),y_vals[int(smoothing_size/2):-int(smoothing_size/2)])
+        y_vals = np.append(y_vals,np.repeat(y_vals[-int(smoothing_size/2)],int(smoothing_size/2)))
 
-            line1.set_xdata(t_vec)
-            line1.set_ydata(y_vals)
+        line1.set_xdata(t_vec)
+        line1.set_ydata(y_vals)
 
-            ax1.set_xlim([np.min(t_vec),np.max(t_vec)])
-            if line1.axes.get_ylim()[0]<0.95*np.min(y_vals) or\
-                np.max(y_vals)>line1.axes.get_ylim()[1] or\
-                np.min(y_vals)<line1.axes.get_ylim()[0]:
-                            ax1.set_ylim([np.min(y_vals),np.max(y_vals)])
-            plt.pause(0.001)
+        ax1.set_xlim([np.min(t_vec),np.max(t_vec)])
+        if line1.axes.get_ylim()[0]<0.95*np.min(y_vals) or\
+            np.max(y_vals)>line1.axes.get_ylim()[1] or\
+            np.min(y_vals)<line1.axes.get_ylim()[0]:
+                        ax1.set_ylim([np.min(y_vals),np.max(y_vals)])
+        plt.pause(0.001)
 
-            peak_locs = np.where(red_grad<-np.std(red_grad))
-            if len(peak_locs[0])==0:
-                continue
+        peak_locs = np.where(red_grad<-np.std(red_grad))
+        if len(peak_locs[0])==0:
+            continue
 
-            prev_pk = peak_locs[0][0]
-            true_peak_locs,pk_loc_span = [],[]
-            for ii in peak_locs[0]:
-                y_pk = y_vals[ii]
-                if (t_vec[ii]-t_vec[prev_pk])<min_time_bw_samps:
-                    pk_loc_span.append(ii)
-                else:
-                    if pk_loc_span==[]:
-                        true_peak_locs.append(ii)
-                    else:
-                        true_peak_locs.append(int(np.mean(pk_loc_span)))
-                        pk_loc_span = []
-
-                prev_pk = int(ii)
-
-            t_peaks = [t_vec[kk] for kk in true_peak_locs]
-            if t_peaks==[]:
-                pass
-                #continue
+        prev_pk = peak_locs[0][0]
+        true_peak_locs,pk_loc_span = [],[]
+        for ii in peak_locs[0]:
+            y_pk = y_vals[ii]
+            if (t_vec[ii]-t_vec[prev_pk])<min_time_bw_samps:
+                pk_loc_span.append(ii)
             else:
-                print('BPM: {0:2.1f}'.format(60.0/np.mean(np.diff(t_peaks))))
-                ax1.set_title('{0:2.0f} BPM'.format(60.0/np.mean(np.diff(t_peaks))),fontsize=24)
-            # plot gradient peaks on original plot to view how BPM is calculated
-            scatter_x,scatter_y = [],[]
-            for jj in true_peak_locs:
-                scatter_x.append(t_vec[jj])
-                scatter_y.append(y_vals[jj])
-                
-            line2.set_data(scatter_x,scatter_y)
+                if pk_loc_span==[]:
+                    true_peak_locs.append(ii)
+                else:
+                    true_peak_locs.append(int(np.mean(pk_loc_span)))
+                    pk_loc_span = []
 
-            plt.pause(0.001)
+            prev_pk = int(ii)
 
-            savefig = input("Save Figure? ")
-            if savefig=='y':
-                plt.savefig('gradient_plot.png',dpi=300,facecolor=[252/255,252/255,252/255])
+        t_peaks = [t_vec[kk] for kk in true_peak_locs]
+        if t_peaks==[]:
+            continue
+        else:
+            print('BPM: {0:2.1f}'.format(60.0/np.mean(np.diff(t_peaks))))
+            ax1.set_title('{0:2.0f} BPM'.format(60.0/np.mean(np.diff(t_peaks))),fontsize=24)
+        # plot gradient peaks on original plot to view how BPM is calculated
+        scatter_x,scatter_y = [],[]
+        for jj in true_peak_locs:
+            scatter_x.append(t_vec[jj])
+            scatter_y.append(y_vals[jj])
+            
+        line2.set_data(scatter_x,scatter_y)
 
-        except KeyboardInterrupt:
-            print("foo")
-            break
-    except KeyboardInterrupt:
-        print("bar")
+        plt.pause(0.001)
+
+        savefig = input("Save Figure? ")
+        if savefig=='y':
+            plt.savefig('gradient_plot.png',dpi=300,facecolor=[252/255,252/255,252/255])
+    except:
+        os.kill(child_pid, signal.SIGKILL)
         break
 
